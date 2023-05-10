a2c11914fca39e23f6220f3879f0cb202cfdb49c
VasylBon <vas.bon.design@gmail.com>
2023-05-10 16:03:02 +0300

    Created promotion timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
+    //Tabs
     let tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -35,4 +36,56 @@ window.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
+
+    //Timer
+    const deadline = "2023-05-11";
+    function getTimeRemaining(endTime) {
+        let days = 0,
+            hours = 0,
+            minutes = 0,
+            seconds = 0;
+        const time = Date.parse(endTime) - Date.parse(new Date());
+
+        if (time > 0) {
+            days = Math.floor(time / 86400000);
+            hours = Math.floor((time / 3600000) % 24);
+            minutes = Math.floor((time / 60000) % 60);
+            seconds = Math.floor((time / 1000) % 60);
+        }
+
+        return { time, days, hours, minutes, seconds };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        }
+        return num;
+    }
+
+    function setClock(selector, endTime) {
+        const time = document.querySelector(selector),
+            days = time.querySelector("#days"),
+            hours = time.querySelector("#hours"),
+            minutes = time.querySelector("#minutes"),
+            seconds = time.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.textContent = getZero(t.days);
+            hours.textContent = getZero(t.hours);
+            minutes.textContent = getZero(t.minutes);
+            seconds.textContent = getZero(t.seconds);
+
+            if (t.time <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(".timer", deadline);
 });
