040cd5c866c392869be8fb1cb0bf6a7e4a6a4c64
VasylBon <vas.bon.design@gmail.com>
2023-05-17 11:38:12 +0300

    Added json server and npm modules

--- js/script.js
+++ js/script.js
@@ -369,10 +369,6 @@ window.addEventListener("DOMContentLoaded", function () {
             statusMessage.src = message.loading;
             form.insertAdjacentElement("afterend", statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open("POST", "server.php");
-
-            request.setRequestHeader("Content-type", "application/json");
             const formData = new FormData(form);
 
             const object = {};
@@ -380,20 +376,23 @@ window.addEventListener("DOMContentLoaded", function () {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener("load", () => {
-                if (request.status === 200) {
-                    console.log(request.response);
+            fetch("server.php", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(object),
+            })
+                .then((data) => data.text())
+                .then((data) => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();
                     statusMessage.remove();
-                } else {
+                })
+                .catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                })
+                .finally(() => {
+                    form.reset();
+                });
         });
     }
 
@@ -419,4 +418,8 @@ window.addEventListener("DOMContentLoaded", function () {
             closeModal();
         }, 4000);
     }
+
+    fetch("db.json")
+        .then((data) => data.json())
+        .then((res) => console.log(res));
 });
